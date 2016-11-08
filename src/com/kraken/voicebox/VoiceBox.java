2f3b1667b7a2d8e2f288c355045603b0c7c2751a
Randall Arms <contact.randallArms@gmail.com>
2016-11-08 15:30:07 -0800

    v0.3 | Added anti-swear filter / censor
    - Command for censor: "/censor <add/remove> <word>"
    - Multi-word censorship functionality to-be-added

--- src/com/kraken/voicebox/VoiceBox.java
+++ src/com/kraken/voicebox/VoiceBox.java
@@ -1,5 +1,5 @@
 // ========================================================================
-// |VOICEBOX v0.2.2
+// |VOICEBOX v0.3
 // | by Kraken | https://www.spigotmc.org/members/kraken_.287802/
 // | code inspired by various Bukkit & Spigot devs -- thank you. 
 // |
@@ -10,10 +10,16 @@
 
 package com.kraken.voicebox;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,11 +27,28 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class VoiceBox extends JavaPlugin {
+	
+	ArrayList<String> shhh = new ArrayList<String>(); 
+    File censorFile = new File("plugins/VoiceBox", "censor.yml");
+    FileConfiguration censor = YamlConfiguration.loadConfiguration(censorFile);
 
     @Override
     public void onEnable() {
     	
         getLogger().info("VoiceBox has been enabled.");
+        
+      //Initialize the censor with a dummy value
+        censor.set("yarbles", true);
+        
+        try {
+			censor.save(censorFile);
+		} catch (IOException e1) {
+			System.out.println("Could not properly initialize censor file; expect possible censor errors.");
+		}
+    
+    	for ( String key : censor.getKeys(false) ) {
+    		shhh.add( key.toLowerCase() );
+        }
         
         //Chat type: 'say' (no command)
         getServer().getPluginManager().registerEvents(new Listener() {
@@ -47,12 +70,26 @@ public class VoiceBox extends JavaPlugin {
 	        		for (Player peep : e.getRecipients()) {
 	        			
 	        			double peepDistance = peep.getLocation().distance(playerLocation);
+	        			boolean isCensored = false;
 	        			
-	        			if (peepDistance <= blockDistance && peep != player) {
-	        				peep.sendMessage(ChatColor.DARK_GREEN + messageBulk + ChatColor.RESET + ChatColor.DARK_GREEN +  "\"" + rawMessage + "\"");
-	                    } else if (peepDistance <= blockDistance && peep == player) {
-	                    	peep.sendMessage(ChatColor.GREEN + messageBulk + ChatColor.RESET + ChatColor.GREEN +  "\"" + rawMessage + "\"");
-	                    }
+	        			//Check the censor
+	        			for ( String badWord : shhh ) {
+	        				
+	        				if ( rawMessage.toLowerCase().contains(badWord) ) {
+	        					player.sendMessage(ChatColor.RED + "You are forbidden from speaking banned phrases.");
+	        					isCensored = true;
+	        					break;
+	        				}
+	        				
+	        			}
+	        				
+	        			if (!isCensored) {
+	        				if (peepDistance <= blockDistance && peep != player) {
+		        				peep.sendMessage(ChatColor.DARK_GREEN + messageBulk + ChatColor.RESET + ChatColor.DARK_GREEN +  "\"" + rawMessage + "\"");
+		                    } else if (peepDistance <= blockDistance && peep == player) {
+		                    	peep.sendMessage(ChatColor.GREEN + messageBulk + ChatColor.RESET + ChatColor.GREEN +  "\"" + rawMessage + "\"");
+		                    }
+	        			}
 	        			
 	                }
         		
@@ -77,7 +114,7 @@ public class VoiceBox extends JavaPlugin {
     	
     	if (sender instanceof Player) {
     		
-    		ChatRelay chat = new ChatRelay(player, args);
+    		ChatRelay chat = new ChatRelay(player, args, shhh);
     		
 	    	switch (command) {
 	    	
@@ -117,6 +154,54 @@ public class VoiceBox extends JavaPlugin {
 	    		case "w":
 	    			
 	    			return chat.whisper();
+	    			
+	    	//Command: censor <add/remove> <phrase>
+	    		case "censor":
+	    			
+	    			String arg;
+	    			
+		    		if ( player.isOp() && args.length > 1) {
+		    			
+		    			switch (args[0]) {
+		    			
+		    				case "add":
+		    					arg = args[1].toLowerCase();
+		    					if ( !shhh.contains( arg ) ) {
+		    						shhh.add( arg );
+		    						censor.set(arg, true);
+		    				        try {
+		    							censor.save(censorFile);
+		    						} catch (IOException e1) {
+		    							System.out.println("Could not properly initialize censor file; expect possible censor errors.");
+		    						}
+		    						player.sendMessage(ChatColor.GREEN + "Phrase added to the censor.");
+		    						return true;	
+		    					}
+		    					
+		    				case "remove":
+		    					arg = args[1].toLowerCase();
+		    					if ( shhh.contains( arg ) ) {
+		    						shhh.remove( arg );
+		    						censor.set(arg, null);
+		    				        try {
+		    							censor.save(censorFile);
+		    						} catch (IOException e1) {
+		    							System.out.println("Could not properly initialize censor file; expect possible censor errors.");
+		    						}
+		    						player.sendMessage(ChatColor.GREEN + "Phrase removed from the censor.");
+		    					} else {
+		    						player.sendMessage(ChatColor.RED + "Phrase not found in the censor.");
+		    					}
+		    					return true;
+		    			
+		    			}
+		    			
+		    		} else if ( args.length <= 1 ) {
+		    			
+		    			player.sendMessage(ChatColor.GRAY + "Unrecognized format, use \"/censor <add/remove> <phrase>\"");	
+						return true;
+						
+		    		}
 	    	        
 	    	}
 	    	
